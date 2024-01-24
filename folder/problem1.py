e468176148f80c8f974b465579ff8eed7f705248
MahmudHyrla1999 <157314458+MahmudHyrla1999@users.noreply.github.com>
2024-01-24 21:23:53 +0200

    Print out addition_result.

--- folder/problem1.py
+++ folder/problem1.py
@@ -1,12 +1,12 @@
 num1 = 25
 num2 = 15
 
-addition_result = num1 + num2
+division_result = num1 + num2
 subtraction_result = num1 - num2
 multiplication_result = num1 * num2
-division_result = num1 / num2
+addition_result = num1 / num2
 
-print(f"Addition: {addition_result}")
+print(f"Division: {division_result}")
 print(f"Subtraction: {subtraction_result}")
 print(f"Multiplication: {multiplication_result}")
-print(f"Division: {division_result}")
+print(f"Addition: {addition_result}")
