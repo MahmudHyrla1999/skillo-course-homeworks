fef6e1bd629ef09cd6d4f0682965cd00851f0b52
MahmudHyrla1999 <157314458+MahmudHyrla1999@users.noreply.github.com>
2024-01-24 20:13:36 +0200

    Initial commit

---
+++ folder/problem1.py
@@ -0,0 +1,12 @@
+num1 = 25
+num2 = 15
+
+addition_result = num1 + num2
+subtraction_result = num1 - num2
+multiplication_result = num1 * num2
+division_result = num1 / num2
+
+print(f"Addition: {addition_result}")
+print(f"Subtraction: {subtraction_result}")
+print(f"Multiplication: {multiplication_result}")
+print(f"Division: {division_result}")
