4c64f31f6dff2a8d2935861d02c97232049f1780
MahmudHyrla1999 <157314458+MahmudHyrla1999@users.noreply.github.com>
2024-01-27 13:47:04 +0200

    first example of loops.

---
+++ control-flow-loops/04control-flow.py
@@ -0,0 +1,18 @@
+username = 'asdf'
+password = 'quest'
+
+if username == 'admin':
+    if password == 'password':
+        print ('Login successful! Welcome admin.')
+    elif password =='123456':
+        print('Weak password.Please reset your password')
+    else:
+        print('Incorrect password.Please try again')
+else:
+    if username == 'quest':
+        if password == 'quest':
+            print('Login successful!.Welcome,quest.')
+        else:
+            print ('Incorrect password.Please try again!')
+    else:
+        print('Unknown user.Please try again later!')
