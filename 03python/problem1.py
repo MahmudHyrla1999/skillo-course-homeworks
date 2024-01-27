711cf68850157d8ea92bb177406397936b201b12
MahmudHyrla1999 <157314458+MahmudHyrla1999@users.noreply.github.com>
2024-01-27 11:40:14 +0200

    Rename folder to python 03

---
+++ 03python/problem1.py
@@ -0,0 +1,13 @@
+num1 = 25
+num2 = 15
+
+division_result = num1 + num2
+subtraction_result = num1 - num2
+multiplication_result = num1 * num2
+addition_result = num1 / num2
+
+print(f"Division: {division_result}")
+print(f"Subtraction: {subtraction_result}")
+print(f"Multiplication: {multiplication_result}")
+print(f"Addition: {addition_result}")
+print(not f"Division: {division_result}")
---
+++ example2.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    if fruit == "cherry":
+        break
+    print(fruit)
+
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    pass
