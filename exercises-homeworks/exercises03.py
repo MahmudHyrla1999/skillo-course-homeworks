b722d8d8dc5766ccacf1b585cb77234bbbe4b902
MahmudHyrla1999 <157314458+MahmudHyrla1999@users.noreply.github.com>
2024-01-28 17:54:25 +0200

    exercises03.py

---
+++ exercises-homeworks/exercises03.py
@@ -0,0 +1,14 @@
+data = input ()
+
+if data == 'banana' or data == 'kiwi' or data == 'apple' :
+    print('fruit')
+
+elif data == 'tomato' or data == 'cucumber' or data == 'pepper':
+    print('vegetables')
+else:
+    print('Unknown')
+
+
+
+
+
