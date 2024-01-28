5644307f36dd18627b9635cc64f18106a58acbc2
MahmudHyrla1999 <157314458+MahmudHyrla1999@users.noreply.github.com>
2024-01-28 13:30:20 +0200

    Numadntext.py

---
+++ exercises-homeworks/NumandText.py
@@ -0,0 +1,14 @@
+num = int(input())
+
+if num == 1:
+    print('one')
+elif num == 2:
+    print('two')
+elif num == 3:
+    print('three')
+
+elif num == 4:
+    print('four')
+
+else:
+    print('Number too big!!!')
