eb2edc5405d5f4dc7c737ba11f8bbfb0674cb066
MahmudHyrla1999 <157314458+MahmudHyrla1999@users.noreply.github.com>
2024-01-28 14:19:21 +0200

    SumSeconda.py

---
+++ exercises-homeworks/SumSeconda.py
@@ -0,0 +1,14 @@
+first = int(input())
+second = int(input())
+third = int(input())
+
+sum = first + second + third
+
+minutes = sum // 60
+seconds = sum % 60
+
+if seconds >= 10:
+    print(f'{minutes}:{seconds}')
+
+else:
+    print(f'{minutes} 0+{seconds}')
